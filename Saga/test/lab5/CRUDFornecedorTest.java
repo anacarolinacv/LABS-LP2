f3678fd799c625851d725243e93fc2b47b1cb3b7
Ana Vasconcelos <ana.vasconcelos@ccc.ufcg.edu.br>
2019-10-01 08:32:55 -0300

    Testes finalizados

--- Saga/test/lab5/CRUDFornecedorTest.java
+++ Saga/test/lab5/CRUDFornecedorTest.java
@@ -49,7 +49,7 @@ class CRUDFornecedorTest {
             this.controle1.exibeFornecedor("Fernanda");
             fail();
         } catch (IllegalArgumentException erro) {
-            assertThrows(IllegalArgumentException.class, () -> this.controle1.exibeFornecedor("Fernanda"));
+            assertEquals("Fornecedor não cadastrado!", erro.getMessage());
         }
     }
 
@@ -66,20 +66,65 @@ class CRUDFornecedorTest {
         this.controle1.cadastraFornecedor("carol", "carol@", "118110388");
         assertEquals("carol - carol@ - 118110388", this.controle1.exibeFornecedor("carol"));
         this.controle1.removeFornecedor("carol");
+        assertThrows(IllegalArgumentException.class, () -> this.controle1.removeFornecedor("fernanda"));
         try {
             this.controle1.exibeFornecedor("carol");
             fail();
         } catch (IllegalArgumentException erro){
-            assertThrows(IllegalArgumentException.class, () -> this.controle1.exibeFornecedor("carol"));
+            assertEquals("Fornecedor não cadastrado!", erro.getMessage());
         }
     }
 
     @Test
     void exibeProdutosFornecedor() {
+        Fornecedor f1 = new Fornecedor("Carol","carol@gmail","33429732");
+        this.controle1.cadastraFornecedor("Carol","carol@gmail","33429732");
+        f1.cadastraProduto("2.50","Suco","Suco de uva");
+        f1.cadastraProduto("3.00","Doce","Suco de morango");
+        f1.exibeProdutosDoFornecedor();
+        try {
+            f1.cadastraProduto("2.50","","Suco de uva");
+            fail();
+        } catch (IllegalArgumentException erro){
+            assertEquals("Nome inválido!", erro.getMessage());
+        }
     }
 
     @Test
     void exibeProdutosTodosFornecedores() {
+
+        Fornecedor f1 = new Fornecedor("Carol","carol@gmail","33429732");
+        this.controle1.cadastraFornecedor("Carol","carol@gmail","33429732");
+        f1.cadastraProduto("2.50","Suco","Suco de uva");
+        f1.cadastraProduto("3.00","Doce","Suco de morango");
+
+        Fornecedor f2 = new Fornecedor("Pedro","pedro@gmail","40028922");
+        this.controle1.cadastraFornecedor("Pedro","pedro@gmail","40028922");
+        f1.cadastraProduto("2.50","Torta","Torta de maça");
+        f1.cadastraProduto("3.00","Doce","Doce de leite");
+
+        this.controle1.exibeProdutosTodosFornecedores();
+
+        try {
+            f1.cadastraProduto("2.50","","Suco de uva");
+            fail();
+        } catch (IllegalArgumentException erro){
+            assertEquals("Nome inválido!", erro.getMessage());
+        }
+        try {
+            f1.cadastraProduto("","Carol","Suco de uva");
+            fail();
+        } catch (IllegalArgumentException erro){
+            assertEquals("Preço inválido!", erro.getMessage());
+        }
+        try {
+            f1.cadastraProduto("2.50","Carol","");
+            fail();
+        } catch (IllegalArgumentException erro){
+            assertEquals("Descrição inválida!", erro.getMessage());
+        }
+
+
     }
 
     @Test
